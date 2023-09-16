bd57fd5893cdef66f5c3256b8da05d90b4fdbcf0
Alejandro <alejandrovelasquezmolina@gmail.com>
2023-09-16 05:22:24 -0500

    feat: se agrega la interacción con JS usando canvas y se construye el grid

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,8 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello {} {}", name, "game of life  with wasm and rust"));
-}
-
-#[wasm_bindgen]
 #[repr(u8)] 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-
 pub enum Cell {
     Dead = 0,
     Alive = 1,
@@ -24,7 +18,7 @@ pub enum Cell {
 #[wasm_bindgen]
 pub struct  Universe {
     width: u32, 
-    heigth: u32,
+    height: u32,
     cells: Vec<Cell>
 }
 
@@ -33,14 +27,14 @@ impl Universe {
         (row * self.width + column) as usize
     }
 
-    fn live_neightbor_count(&self, row: u32, column: u32) -> u8 {
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
         let mut count = 0;
-        for delta_row in [self.heigth - 1, 0, 1].iter().cloned() {
+        for delta_row in [self.height  - 1, 0, 1].iter().cloned() {
            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
             if delta_row == 0 && delta_col == 0 {
                 continue;
             } 
-            let neighbor_row = (row + delta_row) % self.heigth;
+            let neighbor_row = (row + delta_row) % self.height;
             let neighbor_col = (column + delta_col) % self.width;
             let idx = self.get_index(neighbor_row, neighbor_col);
             count += self.cells[idx] as u8;
@@ -85,9 +79,21 @@ impl Universe {
         self.cells = next;
     }
 
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     pub fn new() -> Universe {
         let width = 64;
-        let height = 64;
+        let height  = 64;
 
         let cells = (0..width * height)
             .map(|i| {
